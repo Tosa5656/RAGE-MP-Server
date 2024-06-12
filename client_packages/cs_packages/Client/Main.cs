485eb2556f53ed58f069c3693263aca667589f36
Tosa <127298458+Tosa5656@users.noreply.github.com>
2024-06-12 14:49:23 +0400

    Init project

---
+++ client_packages/cs_packages/Client/Main.cs
@@ -0,0 +1,16 @@
+﻿using RAGE;
+using System;
+using System.Collections;
+
+public class Main : Events.Script
+{
+    public Main()
+    {
+        Events.OnPlayerReady += OnPlayerReady;
+    }
+
+    public void OnPlayerReady()
+    {
+        Chat.Output("Добро пожаловать!");
+    }
+}
---
+++ core/Server/Server/Events.cs
@@ -0,0 +1,10 @@
+﻿using GTANetworkAPI;
+
+public class Events : Script
+{
+    [ServerEvent(Event.ResourceStart)]
+    public void OnResourceStart()
+    {
+        NAPI.Util.ConsoleOutput("Test");
+    }
+}
